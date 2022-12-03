e6df10bbf4582af251fc0bd307a751028135a394
Jean-Francois Simon <jfsimon@startmail.com>
2022-12-03 23:48:12 +0000

    class time

--- src/time.h
+++ src/time.h
@@ -2,23 +2,29 @@
 
 // Count down timer with pre-alarm
 class Timer {
-start(); // Start, does not initialize
-stop(); // Stop, does not initialize
-reset(); // Reset to pre-load, does not change running state
-is_elapsed(); // Timer elapsed (zero or below)
-is_alarm(); // Alarm active (below alarm schedule)
-set_preload(); // Set pre-load value, does not change running states
-set_alarm(); // Set alarm threshold
-get(); // Reads actual value
+  void start();           // Start, does not initialize
+  void stop();            // Stop, does not initialize
+  void reset();           // Reset to pre-load, does not change running state
+  bool is_elapsed();      // Timer elapsed (zero or below)
+  bool is_alarm();        // Alarm active (below alarm schedule)
+  void set_preload();     // Set pre-load value, does not change running states
+  void set_alarm();       // Set alarm threshold
+  chrono::duration get(); // Reads actual value
 public:
-//  Timer(chrono::duration);  // Instanciates running, initialized.
-  Timer();  // Instanciates non running, init to zero.
+  Timer(chrono::duration); // Instanciates running, initialized.
+  Timer();                 // Instanciates non running, init to zero.
   ~Timer();
 }
 
 // Count up chronometer
 class Chronometer {
+  void start();     // Start, does not initialize
+  void stop();      // Stop, does not initialize
+  void reset();     // Reset to zero, does not change running state
+  void set_alarm(); // Set alarm threshold
+  bool is_alarm();  // Alarm active (below alarm schedule)
+  chrono::duration get(); // Reads actual value
 public:
-  Chronometer();
+  Chronometer(); // Starts initialized to zero, not running
   ~Chronometer();
 }
